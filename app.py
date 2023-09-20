b4c1bca1cd19a77b2d1062fb1e1737d948e0e912
JGBeavers <jamesgbeavers@gmail.com>
2023-09-19 23:11:09 -0400

    step four, handle answers

--- app.py
+++ app.py
@@ -11,11 +11,24 @@ responses = []
 
 @app.route('/')
 def home_page():
+    """Display survey name and instructions and button to begin"""
+
     return render_template('home_page.html', survey=survey)
 
 @app.route('/questions/<int:id>')
 def show_questions(id):
+    """Display current question using id and choices, send choice to /answer"""
 
     question = survey.questions[id]
 
     return render_template('questions.html', survey=survey, question=question)
+
+@app.route('/answer', methods=['POST'])
+def get_answer():
+    """Append choice to responses and redirect to next question"""
+
+    choice = request.form['answer']
+    responses.append(choice)
+
+
+    return redirect(f'/questions/{len(responses)}')
